1676b8879dc761dc4f452045f1208276857690b8
S-Stanley <stanleyserbin@gmail.com>
2025-03-09 10:19:51 +0100

    feat(fixtures): hash user password on fixtures #1

--- fixtures/users.sql
+++ fixtures/users.sql
@@ -5,5 +5,5 @@ INSERT INTO "User" (
 ) VALUES (
   'f6cb6e9e-7e19-485c-a4b2-fc10128e4b71',
   'user@music.room',
-  'pass'
+  '$2b$07$9boEzydDEhcHwi12igCt3eRgsop2uWWgDurziwSBmLUNUtSrVbzCC' -- 123
 );
