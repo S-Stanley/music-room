6d04600c1997b362d963ab6650bb9478f6e00a35
Nathan <nathan.bechon@free.fr>
2025-03-13 16:48:12 +0100

    Pop up in Profile

---
+++ services/app/Music Room/ui/PopUpChangeInfo.swift
@@ -0,0 +1,18 @@
+//
+//  PopUpChangeInfo.swift
+//  Music Room
+//
+//  Created by Nathan Bechon on 13/03/2025.
+//
+
+import SwiftUI
+
+struct PopUpChangeInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopUpChangeInfo()
+}
