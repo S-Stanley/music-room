5b9dec07a9e9f3f210b41d11a3c861e0795cca91
Nathan <nathan.bechon@free.fr>
2025-03-13 12:45:38 +0100

    display user email

---
+++ services/app/Music Room/Authentification/ControlScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ControlScreen.swift
+//  Music Room
+//
+//  Created by Nathan Bechon on 12/03/2025.
+//
+
+import SwiftUI
+
+struct ControlScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ControlScreen()
+}
---
+++ services/app/Music Room/ui/BoxSession.swift
@@ -0,0 +1,48 @@
+//
+//  Session.swift
+//  Music Room
+//
+//  Created by Nathan Bechon on 12/03/2025.
+//
+
+import SwiftUI
+
+struct Session: View {
+    var name: String
+    var onJoin: () -> Void
+    
+    var body: some View {
+        HStack {
+                    Text(name) // ✅ Nom dynamique
+                        .font(.headline)
+                        .padding(.leading, 10) // Un peu d'air à gauche
+                        .lineLimit(1) // Limite à une ligne (optionnel)
+                        .truncationMode(.tail) // Coupe si trop long
+
+                    Spacer() // Pousse le bouton à droite
+
+                    Button(action: {
+                        onJoin() // Action à déclencher
+                    }) {
+                        Text("Join")
+                            .padding(.horizontal, 12)
+                            .padding(.vertical, 8)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
+                    .padding(.trailing, 10) // Un peu d'air à droite
+                }
+                .frame(height: 60) // Hauteur fixe
+                .background(Color.gray.opacity(0.2)) // Fond gris clair
+                .cornerRadius(12) // Coins arrondis
+                .padding(.horizontal, 20)
+    }
+}
+
+#Preview {
+    Session(
+        name: "Name 1"
+        onJoin: {}
+    )
+}
