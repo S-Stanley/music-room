935f5f956e04f874792f0fdbac5a27192fb2513c
S-Stanley <stanleyserbin@gmail.com>
2025-03-22 15:29:11 +0100

    feat(fixtures): add one playlist into fixtures #18

---
+++ fixtures/playlists.sql
@@ -0,0 +1,11 @@
+INSERT INTO "Playlist" (
+  id,
+  name,
+  user_id,
+  type
+) VALUES (
+  'e41fa7e7-05a7-4812-9a8b-446ecbc78b2e',
+  'Playlist 1',
+  'f6cb6e9e-7e19-485c-a4b2-fc10128e4b71',
+  'PUBLIC'
+);
--- scripts/reset-local-database.sh
+++ scripts/reset-local-database.sh
@@ -7,5 +7,6 @@ psql -d $DATABASE_URL -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
 npm run migrate:dev --workspace=@music-room/api
 
 psql -d $DATABASE_URL -f ./fixtures/users.sql
+psql -d $DATABASE_URL -f ./fixtures/playlists.sql
 
 echo -e "\nLocal db on $DATABASE_URL is now clean and instancied with fixtures";
