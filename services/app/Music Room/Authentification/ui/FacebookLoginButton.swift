440dd512dcf9017c985494e8335e4555158c4168
Nathan <nathan.bechon@free.fr>
2025-05-12 16:33:37 +0200

    j'arrive passsssssssss facebook c'est nul tfacon

---
+++ services/app/Music Room/Authentification/ui/FacebookLoginButton.swift
@@ -0,0 +1,18 @@
+//
+//  FaceBookLogIn.swift
+//  Music Room
+//
+//  Created by Nathan Bechon on 12/05/2025.
+//
+
+import SwiftUI
+
+struct FaceBookLogIn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FaceBookLogIn()
+}
---
+++ services/app/Music Room/FaceBookViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FaceBookViewModel.swift
+//  Music Room
+//
+//  Created by Nathan Bechon on 12/05/2025.
+//
+
+import Foundation
